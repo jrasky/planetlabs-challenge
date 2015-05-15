6fd95d1e32cd0e889c6c7ef4c8956bf4b2d4dfb6
Jerome Rasky <jyrome.112@gmail.com>
2015-05-14 21:38:24 -0700

    More improvements

--- stocks.py
+++ stocks.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python
-prices = [1.0, 2.0, 3.0, 5.1, 5.5, 1.0]
+prices = [1.0, 2.0, 3.0, 5.1, 5.5, 8.5]
 
 
 def find_points(prices, window):
     best_window = []
     current_window = []
+    skipped = []
     offset = 1
 
+    # ideas to make this better:
+    # start our windows on increases only
+    # consider stopping them on decreases
+    # this would allow for subsets smaller than the window to be considered
+
     for price in prices:
+        print "price:", price
         print "best", best_window
         print "current", current_window
+        print "skipped", skipped
+        print "offset", offset
         print
         if len(current_window) < window:
             current_window.append(price)
         else:
-            # there are some issues with the code below
-            # updon finding a better window, it needs to pop up to
-            # offset and then append the rest of the prices
-            # this does not happen as of yet
             if current_window[-1] - current_window[0] < \
                price - current_window[offset]:
-                current_window.pop(offset - 1)
+                for i in xrange(offset):
+                    current_window.pop(0)
+                current_window.extend(skipped)
                 current_window.append(price)
+                skipped = []
+                offset = 1
             else:
-                offset += 1
                 if offset == window:
                     # we've moved forward an entire window
                     if not best_window or best_window[-1] - best_window[0] < \
                        current_window[-1] - current_window[0]:
                         best_window = current_window
-                    current_window = [price]
+                    current_window = skipped
+                    current_window.append(price)
+                    skipped = []
                     offset = 1
+                else:
+                    offset += 1
+                    skipped.append(price)
 
     print "best", best_window
     print "current", current_window
+    print "skipped", skipped
+    print "offset", offset
+    print
 
     if not best_window or (current_window and best_window[-1] -
                            best_window[0] < current_window[-1] -
@@ -44,7 +60,7 @@ def find_points(prices, window):
 
 
 def main():
-    window = find_points(prices, 2)
+    window = find_points(prices, 3)
 
     print window
 
