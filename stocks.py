752595f65d9a5ca15076ff8cd7f26f87b29b07f5
Jerome Rasky <jyrome.112@gmail.com>
2015-05-17 22:46:45 -0700

    Create back_prices instead of tracking prices
    Makes the space complexity O(w) but WHATEVER

--- stocks.py
+++ stocks.py
@@ -7,16 +7,16 @@ def find_profit(prices, window):
     profit possible if exactly one share is bought then sold within that
     perid. Returns this profit."""
 
+    # back_prices keeps track of previous prices
+    # this is a copy so we don't have to access prices directly
+    back_prices = []
+
     # pivot is the lowest price in the window
     pivot = None
-    # this is the price, so we don't have to index the price list
-    pivot_price = None
 
     # next_pivot is the lowest price in the window after pivot
     # this is where pivot is moved if it falls out of the window
     next_pivot = None
-    # this is the price for it, so we don't have to index like above
-    next_price = None
 
     # accumulated maximum profit
     profit = 0
@@ -24,43 +24,46 @@ def find_profit(prices, window):
     # this is the only direct access of prices, and only assumes that an
     # __iter__ function is available
     for i, price in enumerate(prices):
+        # add the current price to back_prices
+        back_prices.append(price)
+
+        # trim the back prices list to only be the window length
+        while len(back_prices) > window + 1:
+            back_prices.pop(0)
+
         # test to see if we've found a lower pivot
-        if pivot is None or price < pivot_price:
+        if pivot is None or price < back_prices[pivot - i - 1]:
             # set the pivot and the pivot price
             pivot = i
-            pivot_price = price
             # bump the next_pivot if we've passed it
-            if pivot + 1 > next_pivot:
-                next_pivot = pivot + 1
-                next_price = None
-
-        # set the next_price if we're at that index
-        if i == next_pivot:
-            next_price = price
+            next_pivot = max(next_pivot, pivot + 1)
 
         # test to see if we've found a lower next_pivot
-        if next_pivot is None or price < next_price:
+        if next_pivot is None or (next_pivot <= i and price <
+                                  back_prices[next_pivot - i - 1]):
             # set it and the next_price
             next_pivot = i
-            next_price = price
 
         # test to see if the pivot has fallen out of the window
         if i - pivot == window:
             # move the pivot to the next position
             pivot = next_pivot
-            pivot_price = next_price
             # set the next_pivot to one after the new pivot
             next_pivot = pivot + 1
-            next_price = None
 
         # update the profit accumulator
-        profit = max(profit, price - pivot_price)
+        profit = max(profit, price - back_prices[pivot - i - 1])
 
     # return the accumulated profit once done
     return profit
 
 
 class StockProfitTests(unittest.TestCase):
+    def testIncrease(self):
+        self.assertEqual(find_profit([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], 2), 1.0)
+        self.assertEqual(find_profit([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], 3), 2.0)
+        self.assertEqual(find_profit([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], 4), 3.0)
+
     def testSimple(self):
         self.assertEqual(find_profit([1.0, 2.0, 3.0, 1.0, 3.0, 4.0], 5), 3.0)
 
