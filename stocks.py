46161a6a0805e832e915b7b3176e745ad6e2fe3b
Jerome Rasky <jyrome.112@gmail.com>
2015-05-17 18:54:49 -0700

    Add some comments

--- stocks.py
+++ stocks.py
@@ -3,35 +3,60 @@ import unittest
 
 
 def find_profit(prices, window):
+    """Given a certain window size and a list of prices, find the highest
+    profit possible if exactly one share is bought then sold within that
+    perid. Returns this profit."""
+
+    # pivot is the lowest price in the window
     pivot = None
+    # this is the price, so we don't have to index the price list
     pivot_price = None
+
+    # next_pivot is the lowest price in the window after pivot
+    # this is where pivot is moved if it falls out of the window
     next_pivot = None
+    # this is the price for it, so we don't have to index like above
     next_price = None
+
+    # accumulated maximum profit
     profit = 0
 
+    # this is the only direct access of prices, and only assumes that an
+    # __iter__ function is available
     for i, price in enumerate(prices):
+        # test to see if we've found a lower pivot
         if pivot is None or price < pivot_price:
+            # set the pivot and the pivot price
             pivot = i
             pivot_price = price
+            # bump the next_pivot if we've passed it
             if pivot + 1 > next_pivot:
                 next_pivot = pivot + 1
                 next_price = None
 
+        # set the next_price if we're at that index
         if i == next_pivot:
             next_price = price
 
+        # test to see if we've found a lower next_pivot
         if next_pivot is None or price < next_price:
+            # set it and the next_price
             next_pivot = i
             next_price = price
 
+        # test to see if the pivot has fallen out of the window
         if i - pivot == window:
+            # move the pivot to the next position
             pivot = next_pivot
             pivot_price = next_price
+            # set the next_pivot to one after the new pivot
             next_pivot = pivot + 1
             next_price = None
 
+        # update the profit accumulator
         profit = max(profit, price - pivot_price)
 
+    # return the accumulated profit once done
     return profit
 
 
